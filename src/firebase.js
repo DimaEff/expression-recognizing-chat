c500926959e4b8aa26c81f0c13bb1344a7fc35c1
Dima Fominenkov <69797190+DimaEff@users.noreply.github.com>
2022-10-01 14:17:14 +0300

    init firebase

---
+++ src/firebase.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+    apiKey: "AIzaSyCayePJYTxkL3Eqp2qCVb1aSTu1YctH8IY",
+    authDomain: "expression-detection-chat.firebaseapp.com",
+    projectId: "expression-detection-chat",
+    storageBucket: "expression-detection-chat.appspot.com",
+    messagingSenderId: "1093272618395",
+    appId: "1:1093272618395:web:b016b46fbb56c69c26cd3e"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
